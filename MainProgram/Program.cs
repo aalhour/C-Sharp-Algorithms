363caf71625af61a195f853dc3d863a60d153472
Ahmad Alhour <a.z.alhour@gmail.com>
2015-05-21 22:04:41 +0300

    Simple testing Priority Queues.

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -15,7 +15,13 @@ namespace C_Sharp_Algorithms
         static void Main(string[] args)
         {
 			PriorityQueue<int, int, int> priorityQueue = new PriorityQueue<int, int, int> (10);
-			priorityQueue.Enqueue ();
+
+			for (int i = 0; i < 20; ++i)
+			{
+				priorityQueue.Enqueue (i, i, (i/3) + 1);
+			}
+
+			var highest = priorityQueue.Dequeue ();
         }
 
     }
