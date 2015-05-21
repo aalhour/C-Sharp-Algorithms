f11bc838c16e9f2f5338f752c7eb1b4e5112b369
Ahmad Alhour <a.z.alhour@gmail.com>
2015-05-21 18:37:22 +0300

    Tested the new version of HeapSorter

--- MainProgram/AlgorithmsTests/HeapSorterTest.cs
+++ MainProgram/AlgorithmsTests/HeapSorterTest.cs
@@ -13,8 +13,19 @@ namespace C_Sharp_Algorithms.AlgorithmsTests
             int[] numbersList1 = new int[] { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
             List<long> numbersList2 = new List<long> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
 
-            numbersList1.HeapSort();
-            numbersList2.HeapSort();
+            numbersList1.HeapSort ();
+
+			// Sort Ascending (same as the method above);
+			numbersList2.HeapSortAscending ();
+
+			if (numbersList2 [0] > numbersList2 [numbersList2.Count - 1])
+				throw new Exception ();
+
+			// Sort Descending
+            numbersList2.HeapSortDescending ();
+
+			if (numbersList2 [0] < numbersList2 [numbersList2.Count - 1])
+				throw new Exception ();
         }
     }
 }
