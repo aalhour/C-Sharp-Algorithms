0d140fb27126dff3ae31d4ddd3f30d0e065fe619
Ahmad Alhour <a.z.alhour@gmail.com>
2015-05-21 15:29:02 +0300

    Added a blueprint for priority-queues

--- DataStructures/Heap.cs
+++ DataStructures/Heap.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 namespace DataStructures
 {
     /// <summary>
-    /// Implements the Heap Data Structure.
+    /// 
+    /// WORK IN PROGRESS.
+    /// 
+    /// Implements the Priority Queue Data Structure.
     /// </summary>
-    public class Heap<K, V, P>
+    public class PriorityQueue<K, V, P>
     {
         /// <summary>
         /// The Keyed Heap Node object.
@@ -14,23 +17,69 @@ namespace DataStructures
         /// <typeparam name="K"></typeparam>
         /// <typeparam name="V"></typeparam>
         /// <typeparam name="P"></typeparam>
-        public class HeapNode<K, V, P>
+        public class PriorityQueueNode<K, V, P>
         {
             public K Key { get; set; }
             public V Value { get; set; }
             public P Priority { get; set; }
 
-            public HeapNode()
+            public PriorityQueueNode() : this(default(K), default(V), default(P)) 
             {
-
+                // Empty constructor.
             }
 
-            public HeapNode(K key, V value, P priority)
+            public PriorityQueueNode(K key, V value, P priority)
             {
                 this.Key = key;
                 this.Value = value;
                 this.Priority = priority;
             }
+        }//end-of-node-class
+
+
+        /// <summary>
+        /// Instance variables
+        /// </summary>
+        private int _size { get; set; }
+        private ArrayList<PriorityQueueNode<K, V, P>> _collection { get; set; }
+
+
+        //
+        // Inserts a new item with a priority
+        public void InsertWithPriority(V dataItem, P priority)
+        {
+            throw new NotImplementedException();
         }
+
+
+        //
+        // Remove the highest priority element and return it
+        public V PullHighestPriority()
+        {
+            throw new NotImplementedException();
+
+            /*var elementWithHighestPriority = PeekAtHighestPriority();
+            Remove(elementWithHighestPriority);
+
+            return elementWithHighestPriority;*/
+        }
+
+
+        //
+        // Returns the highest priority element
+        public V PeekAtHighestPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //
+        // Removes the first-matched element that has the value = dataItem
+        public void Remove(V dataItem)
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
 }
