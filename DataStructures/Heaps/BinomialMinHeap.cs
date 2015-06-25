5eba30054c46c594982f2aa00dfb2cec97297ec8
Ahmad Alhour <a.z.alhour@gmail.com>
2015-06-25 15:57:03 +0300

    Implemented the _combineTrees method in the BinomialHeap.

--- DataStructures/Heaps/BinomialMinHeap.cs
+++ DataStructures/Heaps/BinomialMinHeap.cs
@@ -1,26 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using DataStructures.Common;
+using DataStructures.Lists;
 
 namespace DataStructures.Heaps
 {
+    /// <summary>
+    /// BINOMIAL MIN HEAP Data Structure
+    /// </summary>
     public class BinomialMinHeap<T> : IMinHeap<T> where T : IComparable<T>
     {
-        public BinomialMinHeap()
+        /// <summary>
+        /// The Heap Node class.
+        /// </summary>
+        private class BinomialNode<T>
         {
+            public T Value { get; set; }
+            public int Degree { get; set; }
+            public BinomialNode<T> Parent { get; set; }
+            public BinomialNode<T> Sibling { get; set; }    // Right-Sibling
+            public BinomialNode<T> Child { get; set; }      // Left-Child
 
+            // Constructors
+            public BinomialNode() : this(default(T), null, null, null) { }
+            public BinomialNode(T value) : this(value, null, null, null) { }
+            public BinomialNode(T value, BinomialNode<T> parent, BinomialNode<T> sibling, BinomialNode<T> child)
+            {
+                Value = value;
+                Parent = parent;
+                Sibling = sibling;
+                Child = child;
+            }
+
+            // Degree increment and decrement
+            public void IncrementDegree() { ++this.Degree; }
+            public void DecrementDegree() { --this.Degree; }
+
+            // Helper boolean flags
+            public bool HasSiblings
+            {
+                get { return this.Sibling != null; }
+            }
+
+            public bool HasChildren
+            {
+                get { return this.Child != null; }
+            }
         }
 
+
+        /// <summary>
+        /// INSTANCE VARIABLES
+        /// </summary>
+        private int _size { get; set; }
+        private const int _defaultCapacity = 8;
+        private ArrayList<BinomialNode<T>> _forest { get; set; }
+
+
+        /// <summary>
+        /// CONSTRUCTORS
+        /// </summary>
+        public BinomialMinHeap() : this(0) { }
+        public BinomialMinHeap(int capacity)
+        {
+            if (capacity <= 0)
+                _forest = new ArrayList<BinomialNode<T>>();
+            else
+                _forest = new ArrayList<BinomialNode<T>>(capacity);
+
+            _size = 0;
+        }
+
+
+        /************************************************************************************************/
+        /** PRIVATE HELPER FUNCTIONS                                                                    */
+
+        /// <summary>
+        /// Returns index of the tree with the minimum root's value.
+        /// </summary>
+        private int _findMinIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Combines two trees and returns the new tree root node.
+        /// </summary>
+        private BinomialNode<T> _combineTrees(BinomialNode<T> firstTreeRoot, BinomialNode<T> secondTreeRoot)
+        {
+            if(firstTreeRoot == null || secondTreeRoot == null)
+                throw new ArgumentNullException("Either one of the nodes or both are null.");
+
+            if (secondTreeRoot.Value.IsLessThan(firstTreeRoot.Value))
+                return _combineTrees(secondTreeRoot, firstTreeRoot);
+
+            secondTreeRoot.Sibling = firstTreeRoot.Child;
+            firstTreeRoot.Child = secondTreeRoot;
+            return firstTreeRoot;
+        }
+
+        /// <summary>
+        /// Clones a tree, given it's root node.
+        /// </summary>
+        private BinomialNode<T> _cloneTree(BinomialNode<T> treeRoot)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /************************************************************************************************/
+        /** PUBLIC API FUNCTIONS                                                                        */
+
+
+        /// <summary>
+        /// Returns count of elements in heap.
+        /// </summary>
         public int Count()
         {
-            throw new NotImplementedException();
+            return _size;
         }
 
+        /// <summary>
+        /// Checks if heap is empty
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return (_size == 0);
         }
 
         public void Heapify(IList<T> newCollection)
@@ -48,7 +155,7 @@ namespace DataStructures.Heaps
             throw new NotImplementedException();
         }
 
-        public void Clear()
+        public void Merge(BinomialMinHeap<T> otherHeap)
         {
             throw new NotImplementedException();
         }
@@ -63,9 +170,16 @@ namespace DataStructures.Heaps
             throw new NotImplementedException();
         }
 
-        public IMaxHeap<T> ToBinaryMaxHeap()
+        public IMaxHeap<T> ToMaxHeap()
         {
             throw new NotImplementedException();
         }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
 }
