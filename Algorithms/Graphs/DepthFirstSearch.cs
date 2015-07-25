e70c3422137cd6e7f8e5f080be3674ba56f4ae6e
Ahmad Alhour <a.z.alhour@gmail.com>
2015-07-25 16:06:45 +0300

    Added a new file for DFS algorithms

---
+++ Algorithms/Graphs/DepthFirstSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DataStructures.Graphs;
+
+namespace Algorithms
+{
+	public static class DepthFirstSearch
+	{
+		private static void DFSRecursive<T>(IUndirectedGraph<T> Graph, T fromVertex) where T : IComparable<T>
+		{
+			
+		}
+
+		public static string VisitAll<T> (IUndirectedGraph<T> Graph, T startVertex) where T : IComparable<T>
+		{
+			string output = string.Empty;
+			Dictionary<T, T> parents = new Dictionary<T, T> ();
+
+			foreach (var vertex in Graph.Vertices)
+			{
+				if (!parents.ContainsKey (vertex))
+				{
+					parents.Add (vertex, default(T));
+					output = String.Format ("{0}({1}) -> ", output, vertex);
+					DFSRecursive (Graph, vertex);
+				}
+			}
+
+			return output;
+		}
+
+		public static T Find<T> (IUndirectedGraph<T> Graph, T startVertex) where T : IComparable<T>
+		{
+			return default(T);
+		}
+	}
+}
+
