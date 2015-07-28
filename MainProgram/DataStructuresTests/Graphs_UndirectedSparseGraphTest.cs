9f15244d5a2fc4ead95e42bcdaba1e0325054192
Ahmad Alhour <a.z.alhour@gmail.com>
2015-07-28 12:21:22 +0300

    Reformatted the graphs tests.

---
+++ MainProgram/DataStructuresTests/Graphs_UndirectedSparseGraphTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+
+using DataStructures.Graphs;
+
+namespace C_Sharp_Algorithms.DataStructuresTests
+{
+    public static class Graphs_UndirectedSparseGraphTest
+    {
+        public static void DoTest()
+        {
+            var graph = new UndirectedSparseGraph<string>();
+
+            var verticesSet1 = new string[] { "a", "z", "s", "x", "d", "c", "f", "v" };
+
+            graph.AddVertices(verticesSet1);
+
+            graph.AddEdge("a", "s");
+            graph.AddEdge("a", "z");
+            graph.AddEdge("s", "x");
+            graph.AddEdge("x", "d");
+            graph.AddEdge("x", "c");
+            graph.AddEdge("d", "f");
+            graph.AddEdge("d", "c");
+            graph.AddEdge("c", "f");
+            graph.AddEdge("c", "v");
+            graph.AddEdge("v", "f");
+
+            Debug.Assert(graph.VerticesCount == 8, "Wrong vertices count.");
+            Debug.Assert(graph.EdgesCount == 10, "Wrong edges count.");
+
+            Console.WriteLine("[*] Undirected Sparse Graph:");
+            Console.WriteLine("Graph nodes and edges:");
+            Console.WriteLine(graph.ToReadable() + "\r\n");
+
+            graph.RemoveEdge("d", "c");
+            graph.RemoveEdge("c", "v");
+            graph.RemoveEdge("a", "z");
+            Debug.Assert(graph.VerticesCount == 8, "Wrong vertices count.");
+            Debug.Assert(graph.EdgesCount == 7, "Wrong edges count.");
+
+            Console.WriteLine("After removing edges (d-c), (c-v), (a-z):");
+            Console.WriteLine(graph.ToReadable() + "\r\n");
+
+            graph.RemoveVertex("x");
+            Debug.Assert(graph.VerticesCount == 7, "Wrong vertices count.");
+            Debug.Assert(graph.EdgesCount == 4, "Wrong edges count.");
+
+            Console.WriteLine("After removing node (x):");
+            Console.WriteLine(graph.ToReadable() + "\r\n");
+
+            graph.AddVertex("x");
+            graph.AddEdge("s", "x");
+            graph.AddEdge("x", "d");
+            graph.AddEdge("x", "c");
+            graph.AddEdge("d", "c");
+            graph.AddEdge("c", "v");
+            graph.AddEdge("a", "z");
+            Console.WriteLine("Re-added the deleted vertices and edges to the graph.");
+            Console.WriteLine(graph.ToReadable() + "\r\n");
+
+            Console.WriteLine("Walk the graph using BFS:");
+            graph.BreadthFirstWalk("s");		// output: (s) (a) (x) (z) (d) (c) (f) (v)
+            Console.WriteLine("\r\n");
+
+
+            /********************************************************************/
+
+
+            graph.Clear();
+            Console.WriteLine("Cleared the graph from all vertices and edges.\r\n");
+
+            var verticesSet2 = new string[] { "a", "b", "c", "d", "e", "f" };
+
+            graph.AddVertices(verticesSet2);
+
+            graph.AddEdge("a", "b");
+            graph.AddEdge("a", "d");
+            graph.AddEdge("b", "e");
+            graph.AddEdge("d", "b");
+            graph.AddEdge("d", "e");
+            graph.AddEdge("e", "c");
+            graph.AddEdge("c", "f");
+            graph.AddEdge("f", "f");
+
+            Debug.Assert(graph.VerticesCount == 6, "Wrong vertices count.");
+            Debug.Assert(graph.EdgesCount == 8, "Wrong edges count.");
+
+            Console.WriteLine("[*] NEW Undirected Sparse Graph:");
+            Console.WriteLine("Graph nodes and edges:");
+            Console.WriteLine(graph.ToReadable() + "\r\n");
+
+            Console.WriteLine("Walk the graph using DFS:");
+            graph.DepthFirstWalk();		// output: (a) (b) (e) (d) (c) (f) 
+        }
+    }
+}
+
---
+++ MainProgram/DataStructuresTests/HashTableSeparateChainingTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using DataStructures.Dictionaries;
+
+namespace C_Sharp_Algorithms.DataStructuresTests
+{
+    public static class HashTableSeparateChainingTest
+    {
+        public static void DoTest()
+        {
+            var studentsMarks = new ChainedHashTable<string, int>();
+
+            //
+            // TEST ADD KEY-VALUE PAIRS
+            studentsMarks.Add("Konstantinos", 124);
+            studentsMarks.Add("Bic", 224);
+            studentsMarks.Add("Z", 324);
+            studentsMarks.Add("Ioanna", 424);
+            studentsMarks.Add("Mark Zuckerberg", 524);
+            studentsMarks.Add("Semsem", 624);
+            studentsMarks.Add("Sa3eeed", 724);
+            studentsMarks.Add("Sameer", 824);
+            studentsMarks.Add("Ahmad", 924);
+            studentsMarks.Add("Zeyad", 999);
+            studentsMarks.Add("Mahmoood 3eed", 111);
+            studentsMarks.Add("Mahmoood Abu 3eed", 222);
+            studentsMarks.Add("EISA", 333);
+            studentsMarks.Add("Test1", 3210);
+            studentsMarks.Add("Test11", 3210);
+            studentsMarks.Add("Test222", 3210);
+            studentsMarks.Add("Test3333", 3210);
+            studentsMarks.Add("Test44444", 3210);
+            studentsMarks.Add("Test555555", 3210);
+            studentsMarks.Add("Test6666666", 3210);
+            studentsMarks.Add("Test77777777", 3210);
+            studentsMarks.Add("Test888888888", 3210);
+
+            //
+            // TEST FETCH KEY-VALUE
+            var mark = studentsMarks["Ahmad"];
+            Debug.Assert(mark == 924);
+
+            mark = studentsMarks["Konstantinos"];
+            Debug.Assert(mark == 124);
+
+            mark = studentsMarks["Bic"];
+            Debug.Assert(mark == 224);
+
+            mark = studentsMarks["Z"];
+            Debug.Assert(mark == 324);
+
+            mark = studentsMarks["Ioanna"];
+            Debug.Assert(mark == 424);
+
+            mark = studentsMarks["Mark Zuckerberg"];
+            Debug.Assert(mark == 524);
+
+            mark = studentsMarks["Semsem"];
+            Debug.Assert(mark == 624);
+
+            mark = studentsMarks["Sa3eeed"];
+            Debug.Assert(mark == 724);
+
+            mark = studentsMarks["Sameer"];
+            Debug.Assert(mark == 824);
+
+            mark = studentsMarks["Zeyad"];
+            Debug.Assert(mark == 999);
+
+            mark = studentsMarks["Mahmoood 3eed"];
+            Debug.Assert(mark == 111);
+
+            mark = studentsMarks["Mahmoood Abu 3eed"];
+            Debug.Assert(mark == 222);
+
+            mark = studentsMarks["EISA"];
+            Debug.Assert(mark == 333);
+
+            //
+            // TEST DELETE BY KEYS
+            studentsMarks.Remove("Ahmad");
+            studentsMarks.Remove("Zeyad");
+            studentsMarks.Remove("Bic");
+            studentsMarks.Remove("Konstantinos");
+            studentsMarks.Remove("Sameer");
+            studentsMarks.Remove("Z");
+            studentsMarks.Remove("Ioanna");
+            studentsMarks.Remove("Mark Zuckerberg");
+            studentsMarks.Remove("Semsem");
+            studentsMarks.Remove("Sa3eeed");
+            studentsMarks.Remove("Test1");
+            studentsMarks.Remove("Test11");
+            studentsMarks.Remove("Test222");
+            studentsMarks.Remove("Test3333");
+            studentsMarks.Remove("Test44444");
+            studentsMarks.Remove("Test555555");
+            studentsMarks.Remove("Test6666666");
+            studentsMarks.Remove("Test77777777");
+            studentsMarks.Remove("Test888888888");
+
+            Debug.Assert(studentsMarks.Count == 3);
+
+            KeyValuePair<string, int>[] array = new KeyValuePair<string, int>[studentsMarks.Count];
+            studentsMarks.CopyTo(array, 0);
+
+            Debug.Assert(array.Length == studentsMarks.Count);
+        }
+    }
+}
