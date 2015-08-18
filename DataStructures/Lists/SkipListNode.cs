b458882cf476f303d656cfb30a4d0f64371fc058
Ahmad Alhour <a.z.alhour@gmail.com>
2015-08-18 17:14:18 +0300

    SkipLists: Completed simple implementation and first round of testing.

---
+++ DataStructures/Lists/SkipListNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DataStructures.Lists
+{
+    public class SkipListNode<T> : IComparable<SkipListNode<T>> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Instance variables
+        /// </summary>
+        private T _value;
+        private SkipListNode<T>[] _forwards;
+        private const int _defaultLevel = 8;
+
+        /// <summary>
+        /// CONSTRUCTORS
+        /// </summary>
+        public SkipListNode(T value) : this(value, _defaultLevel) { }
+
+        public SkipListNode(T value, int level)
+        {
+            level = level <= 0 ? _defaultLevel : level;
+
+            Value = value;
+            Forwards = new SkipListNode<T>[level];
+        }
+
+        /// <summary>
+        /// Get and set node's value
+        /// </summary>
+        public virtual T Value
+        {
+            get { return this._value; }
+            private set { this._value = value; }
+        }
+
+        /// <summary>
+        /// Get and set node's forwards links
+        /// </summary>
+        public virtual SkipListNode<T>[] Forwards
+        {
+            get { return this._forwards; }
+            private set { this._forwards = value; }
+        }
+
+        /// <summary>
+        /// Return level of node.
+        /// </summary>
+        public virtual int Level
+        {
+            get { return Forwards.Length; }
+        }
+
+        /// <summary>
+        /// IComparable method implementation
+        /// </summary>
+        public int CompareTo(SkipListNode<T> other)
+        {
+            if (other == null)
+                return -1;
+
+            return this.Value.CompareTo(other.Value);
+        }
+    }
+}
---
+++ MainProgram/DataStructuresTests/SkipListTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+using DataStructures.Lists;
+
+namespace C_Sharp_Algorithms.DataStructuresTests
+{
+    public static class SkipListTest
+    {
+        public static void DoTest()
+        {
+            var skipList = new SkipList<int>();
+            skipList.Add(10);
+            skipList.Add(5);
+            skipList.Add(20);
+            skipList.Add(11);
+            skipList.Add(1);
+
+            // Get enumarator
+            var enumerator = skipList.GetEnumerator();
+
+            Console.WriteLine(" [*] Skip-List elements:");
+            Console.Write("..... ");
+            while (enumerator.MoveNext())
+                Console.Write("{0} -> ", enumerator.Current);
+
+            Console.WriteLine("\r\n===================================\r\n");
+
+            int min = default(int);
+            if (skipList.TryDeleteMin(out min))
+                Console.WriteLine("Removed min from SkipList, and it was: {0}", min);
+
+            Console.WriteLine("\r\n===================================\r\n");
+
+            // Reload enumarator
+            enumerator = skipList.GetEnumerator();
+
+            Console.WriteLine(" [*] Skip-List elements:");
+            Console.Write("..... ");
+            while (enumerator.MoveNext())
+                Console.Write("{0} -> ", enumerator.Current);
+
+            Console.WriteLine("\r\n===================================\r\n");
+
+            Console.ReadLine();
+        }
+    }
+}
