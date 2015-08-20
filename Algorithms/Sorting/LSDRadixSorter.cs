21dce3d69111fceded17b5f37b72837e7f9e0664
Ahmad Alhour <a.z.alhour@gmail.com>
2015-08-20 12:31:35 +0300

    Implemented the LSD Radix Sort for strings, characater arrays, and collections of strings.

---
+++ Algorithms/Sorting/LSDRadixSorter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Algorithms.Common;
+
+namespace Algorithms.Sorting
+{
+    /// <summary>
+    /// LSD (Least Significat Digit) Radix Sort.
+    ///
+    /// Sorts ASCII-encoded strings.
+    /// Implemented as a static class of extension methods.
+    /// </summary>
+    public static class LSDRadixSorter
+    {
+        /// <summary>
+        /// Extension method for sorting strings.
+        /// </summary>
+        public static string LSDRadixSort(this string source)
+        {
+            if (string.IsNullOrEmpty(source) || source.Length <= 1)
+                return source;
+
+            // LSD Radix Sort the character arrapy representation of the string
+            var charArray = source.ToCharArray();
+            charArray.LSDRadixSort();
+
+            return new String(charArray);
+        }
+
+
+        /// <summary>
+        /// Extension method for sorting character arrays, in place.
+        /// </summary>
+        public static void LSDRadixSort(this char[] source)
+        {
+            if (source == null || source.Length <= 1)
+                return;
+
+            // extend ASCII alphabet size
+            int asciiSize = 256;
+
+            int length = source.Length;
+            char[] auxiliary = new char[length];
+
+            // compute frequency counts
+            int[] count = new int[asciiSize + 1];
+
+            for (int i = 0; i < length; i++)
+                count[source[i] + 1]++;
+
+            // compute cumulates
+            for (int r = 0; r < asciiSize; r++)
+                count[r + 1] += count[r];
+
+            // move data
+            for (int i = 0; i < length; i++)
+                auxiliary[count[source[i]]++] = source[i];
+
+            // copy back
+            for (int i = 0; i < length; i++)
+                source[i] = auxiliary[i];
+        }
+
+
+        /// <summary>
+        /// Extension method for sorting collections of strings of the same width, in place.
+        /// </summary>
+        public static void LSDRadixSort(this IList<String> collection, int stringFixedWidth)
+        {
+            // Validate input
+            if (collection == null || collection.Count <= 1)
+                return;
+            else
+            {
+                for (int i = 0; i < collection.Count; ++i)
+                    if (collection[i] == null || collection[i].Length != stringFixedWidth)
+                        throw new ApplicationException("Not all strings have the same width");
+            }
+
+            // extend ASCII alphabet size
+            int asciiSize = 256;
+
+            int stringsCount = collection.Count;
+            string[] auxiliary = new string[stringsCount];
+
+            for (int d = stringFixedWidth - 1; d >= 0; d--)
+            {
+                // compute frequency counts
+                int[] count = new int[asciiSize + 1];
+
+                for (int i = 0; i < stringsCount; i++)
+                    count[collection[i][d] + 1]++;
+
+                // compute cumulates
+                for (int r = 0; r < asciiSize; r++)
+                    count[r + 1] += count[r];
+
+                // move data
+                for (int i = 0; i < stringsCount; i++)
+                    auxiliary[count[collection[i][d]]++] = collection[i];
+
+                // copy back
+                for (int i = 0; i < stringsCount; i++)
+                    collection[i] = auxiliary[i];
+            }
+        }
+    }
+}
---
+++ MainProgram/AlgorithmsTests/LSDRadixSorterTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using Algorithms.Sorting;
+
+namespace C_Sharp_Algorithms.AlgorithmsTests
+{
+    public static class LSDRadixSorterTest
+    {
+        public static void DoTest()
+        {
+            //
+            // Sort strings
+            var name1 = "Mr. Ahmad Alhour";
+            var name2 = "Msr. Anna John Hopcraft";
+            var number1 = "0987654321";
+            var number2 = "000999888777111222333777666555";
+
+            name1 = (name1.LSDRadixSort()).Trim();
+            Debug.Assert(name1 == ".AAMadhhlmorru");
+
+            name2 = (name2.LSDRadixSort()).Trim();
+            Debug.Assert(name2 == ".AHJMaacfhnnnooprrst");
+
+            number1 = number1.LSDRadixSort();
+            Debug.Assert(number1 == "0123456789");
+
+            number2 = number2.LSDRadixSort();
+            Debug.Assert(number2 == "000111222333555666777777888999");
+
+            //
+            // Sort a list of strings of the same length
+            var toBeSorted = new List<string>() { "ahmad", "ahmed", "johny", "ammy1", "ammy2", "zeyad", "aliaa", "aaaaa", "mmmmm", "zzzzz" };
+            var alreadySorted = new List<string>() { "aaaaa", "ahmad", "ahmed", "aliaa", "ammy1", "ammy2", "johny", "mmmmm", "zeyad", "zzzzz" };
+
+            toBeSorted.LSDRadixSort(stringFixedWidth: 5);
+
+            for (int i = 0; i < toBeSorted.Count; ++i)
+            {
+                Debug.Assert(toBeSorted[i] == alreadySorted[i]);
+            }
+
+        }
+
+    }
+
+}
