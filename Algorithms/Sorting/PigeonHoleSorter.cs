96ee270f54c709426a7121e0a6274cdc9b60726b
Lucas Lemaire <zwormi@gmail.com>
2015-10-07 13:23:28 +0200

    Add PigeonHole sort algorithm with test

---
+++ Algorithms/Sorting/PigeonHoleSorter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sorting
+{
+    /// <summary>
+    /// Only support IList<int> Sort
+    /// Also called CountSort (not CountingSort)
+    /// </summary>
+    public static class PigeonHoleSorter
+    {
+        public static void PigeonHoleSort(this IList<int> collection)
+        {
+            collection.PigeonHoleSortAscending();
+        }
+
+        public static void PigeonHoleSortAscending(this IList<int> collection)
+        {
+            int min = collection.Min();
+            int max = collection.Max();
+            int size = max - min + 1;
+            int[] holes = new int[size];
+            foreach (int x in collection)
+            {
+                holes[x - min]++;
+            }
+            
+            int i = 0;
+            for (int count = 0; count < size; count++)
+            {
+                while (holes[count]-- > 0)
+                {
+                    collection[i] = count + min;
+                    i++;
+                }
+            }    
+        }
+
+        public static void PigeonHoleSortDescending(this IList<int> collection)
+        {
+            int min = collection.Min();
+            int max = collection.Max();
+            int size = max - min + 1;
+            int[] holes = new int[size];
+            foreach (int x in collection)
+            {
+                holes[x - min]++;
+            }
+
+            int i = 0;
+            for (int count = size-1; count >= 0; count--)
+            {
+                while (holes[count]-- >0)
+                {
+                    collection[i] = count + min;
+                    i++;
+                }
+            }
+        }
+    }
+}
