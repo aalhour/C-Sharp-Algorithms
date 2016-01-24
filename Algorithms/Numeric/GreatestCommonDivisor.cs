ed0a59b2a409bbf6a228a7694faf72dd7309b9f3
Ferhat <ffrhtd@hotmail.com>
2016-01-24 14:13:12 +0200

    added euclidean algorithm for greatest common divisor

---
+++ Algorithms/Numeric/GreatestCommonDivisor.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Numeric
+{
+    public static class GreatestCommonDivisor
+    {
+        public static uint FindGCD(uint a, uint b)
+        {
+            if (a == 0)
+                return b;
+            else if (b == 0)
+                return a;
+
+            uint _a = a, _b = b;
+
+            uint q = _a / _b;
+            uint r = _a % _b;
+
+            while(r != 0)
+            {
+                _a = _b;
+                _b = r;
+                r = _a % _b;
+            }
+
+            return _b;
+        }
+
+        public static bool IsRelativelyPrime(uint a, uint b)
+        {
+            return FindGCD(a, b) == 1;
+        }
+    }
+}
