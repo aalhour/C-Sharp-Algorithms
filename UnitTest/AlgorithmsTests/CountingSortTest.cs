967d23902eda581af28e8d612cb4bb62b576b99c
Ivandro Ismael <ivandrofly@gmail.com>
2017-05-30 20:41:34 +0000

    [CountingSortTest] - add assert

--- UnitTest/AlgorithmsTests/CountingSortTest.cs
+++ UnitTest/AlgorithmsTests/CountingSortTest.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 
 using Algorithms.Sorting;
 using Xunit;
+using System.Linq;
 
 namespace UnitTest.AlgorithmsTests
 {
@@ -13,6 +14,7 @@ namespace UnitTest.AlgorithmsTests
         {
             int[] numbersList1 = new int[] { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
             numbersList1.CountingSort();
+            Assert.True(numbersList1.SequenceEqual(new[] { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 }));
         }
     }
 }
