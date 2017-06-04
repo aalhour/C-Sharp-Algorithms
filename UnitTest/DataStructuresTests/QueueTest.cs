db671b361a6824beb5bd8c1ff030c6bd7fcab068
Ivandro Ismael <ivandrofly@hotmail.com>
2017-06-04 21:42:44 +0000

    [QueueTest] - fix unit-test

--- UnitTest/DataStructuresTests/QueueTest.cs
+++ UnitTest/DataStructuresTests/QueueTest.cs
@@ -2,48 +2,44 @@
 using System.Diagnostics;
 
 using DataStructures.Lists;
+using Xunit;
 
 namespace C_Sharp_Algorithms.DataStructuresTests
 {
-	public static class QueueTest
-	{
-		public static void DoTest ()
-		{
-			string top;
-			Queue<string> queue = new Queue<string>();
+    public static class QueueTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            string top;
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("aaa");
+            queue.Enqueue("bbb");
+            queue.Enqueue("ccc");
+            queue.Enqueue("ddd");
+            queue.Enqueue("eee");
+            queue.Enqueue("fff");
+            queue.Enqueue("ggg");
+            queue.Enqueue("hhh");
+            Assert.Equal(queue.Count, 8);
 
-			queue.Enqueue("aaa");
-			queue.Enqueue("bbb");
-			queue.Enqueue("ccc");
-			queue.Enqueue("ddd");
-			queue.Enqueue("eee");
-			queue.Enqueue("fff");
-			queue.Enqueue("ggg");
-			queue.Enqueue("hhh");
-			Debug.Assert(queue.Count == 8);
+            var array = queue.ToArray();
+            // fails if wrong size
+            Assert.Equal(array.Length, 8);
 
-			Console.WriteLine("Queue Elements:\r\n" + queue.ToHumanReadable());
+            queue.Dequeue();
+            queue.Dequeue();
+            top = queue.Dequeue();
+            Assert.Equal(top, "ccc");
 
-			var array = queue.ToArray();
-			Debug.Assert (array.Length == 8, "Wrong size.");
+            queue.Dequeue();
+            queue.Dequeue();
+            Assert.Equal(queue.Top, "fff");
 
-			queue.Dequeue();
-			queue.Dequeue();
-			top = queue.Dequeue();
-			Debug.Assert (top == "ccc");
-
-			Console.WriteLine("Old 3nd-last: " + top + "\r\n");
-			Console.WriteLine("Queue Elements:\r\n" + queue.ToHumanReadable());
-
-			queue.Dequeue();
-			queue.Dequeue();
-			Debug.Assert (queue.Top == "fff");
-
-			Console.WriteLine("Queue Elements:\r\n" + queue.ToHumanReadable());
-
-			var array2 = queue.ToArray();
-			Debug.Assert (array2.Length == 3, "Wrong size.");
-		}
-	}
+            var array2 = queue.ToArray();
+            // fails if wrong size
+            Assert.Equal(array2.Length, 3);
+        }
+    }
 }
 
