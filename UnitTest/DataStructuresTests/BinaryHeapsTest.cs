1a8931d6dab2ece1176cd2c0727fc6f98589cd01
Ivandro Ismael <ivandrofly@hotmail.com>
2017-08-23 19:14:16 +0100

    [BinaryHeapsTest - fix unit-test

--- UnitTest/DataStructuresTests/BinaryHeapsTest.cs
+++ UnitTest/DataStructuresTests/BinaryHeapsTest.cs
@@ -1,44 +1,43 @@
-﻿using System;
-using System.Diagnostics;
-using System.Collections.Generic;
-
+﻿using Algorithms.Sorting;
 using DataStructures.Heaps;
-using Algorithms.Sorting;
+using System.Collections.Generic;
+using Xunit;
 
-namespace C_Sharp_Algorithms.DataStructuresTests
+namespace UnitTest.DataStructuresTests
 {
-	public static class MinHeapTest
-	{
-		public static void DoTest ()
-		{
-			BinaryMinHeap<long> minHeap = new BinaryMinHeap<long> (Comparer<long>.Default);
+    public static class MinHeapTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            BinaryMinHeap<long> minHeap = new BinaryMinHeap<long>(Comparer<long>.Default);
 
-			minHeap.Add (23);
-			minHeap.Add (42);
-			minHeap.Add (4);
-			minHeap.Add (16);
-			minHeap.Add (8);
-			minHeap.Add (15);
-			minHeap.Add (9);
-			minHeap.Add (55);
-			minHeap.Add (0);
-			minHeap.Add (34);
-			minHeap.Add (12);
-			minHeap.Add (2);
-			minHeap.Add (93);
-			minHeap.Add (14);
-			minHeap.Add (27);
+            minHeap.Add(23);
+            minHeap.Add(42);
+            minHeap.Add(4);
+            minHeap.Add(16);
+            minHeap.Add(8);
+            minHeap.Add(15);
+            minHeap.Add(9);
+            minHeap.Add(55);
+            minHeap.Add(0);
+            minHeap.Add(34);
+            minHeap.Add(12);
+            minHeap.Add(2);
+            minHeap.Add(93);
+            minHeap.Add(14);
+            minHeap.Add(27);
 
-			var array = minHeap.ToArray ();
-			Debug.Assert (array.Length == minHeap.Count, "Wrong size.");
+            var array = minHeap.ToArray();
+            Assert.True(array.Length == minHeap.Count, "Wrong size.");
 
-			var list = minHeap.ToList ();
-			Debug.Assert (list.Count == minHeap.Count, "Wrong size.");
+            var list = minHeap.ToList();
+            Assert.True(list.Count == minHeap.Count, "Wrong size.");
 
-			array.HeapSortDescending();
-			var maxHeap = minHeap.ToMaxHeap ();
-			Debug.Assert (maxHeap.Peek() == array[0], "Wrong maximum.");
-		}
-	}
+            array.HeapSortDescending();
+            var maxHeap = minHeap.ToMaxHeap();
+            Assert.True(maxHeap.Peek() == array[0], "Wrong maximum.");
+        }
+    }
 }
 
