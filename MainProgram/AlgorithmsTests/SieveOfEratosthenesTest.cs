149321ab425495c7f5c2c899213f53bae5aec040
maxm2017 <mmercel8@gmail.com>
2017-10-15 17:24:16 +0100

    Sieve of Eratosthenes test
    Tests the Sieve of Eratosthenese implementation

---
+++ MainProgram/AlgorithmsTests/SieveOfEratosthenesTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Algorithms.AlgorithmsTests
+{
+    class SieveOfEratosthenesTest
+    {
+        public static void DoTest()
+        {
+            foreach (int i in Algorithms.Numeric.SieveOfEratosthenes.GeneratePrimesUpTo(100))
+            {
+                Console.Write(i + " ");
+            }
+
+            foreach (int i in Algorithms.Numeric.SieveOfEratosthenes.GeneratePrimesUpTo(-100))
+            {
+                Console.Write(i + " ");
+            }
+
+            foreach (int i in Algorithms.Numeric.SieveOfEratosthenes.GeneratePrimesUpTo(2))
+            {
+                Console.Write(i + " ");
+            }
+
+            foreach (int i in Algorithms.Numeric.SieveOfEratosthenes.GeneratePrimesUpTo(1))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
