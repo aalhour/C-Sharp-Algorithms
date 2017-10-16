c113ce04403f5a1f347630c115d30fc9ef9b4c7d
maxm2017 <mmercel8@gmail.com>
2017-10-16 17:54:55 +0100

    Added Sieve of Eratosthenes test
    Added Sieve of Eratosthenes test

--- MainProgram/AlgorithmsTests/SieveOfEratosthenesTest.cs
+++ MainProgram/AlgorithmsTests/SieveOfEratosthenesTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_Sharp_Algorithms.AlgorithmsTests
 {
