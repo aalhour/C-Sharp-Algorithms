330905a58afd838ff73c0a917979791f1b8ca3b1
Ivandro Ismael <ivandrofly@hotmail.com>
2017-11-22 20:31:38 +0000

    [InsertionSortTest] - fix unit-test

--- UnitTest/AlgorithmsTests/InsertionSortTest.cs
+++ UnitTest/AlgorithmsTests/InsertionSortTest.cs
@@ -3,54 +3,63 @@ using System.Collections.Generic;
 
 using DataStructures.Lists;
 using Algorithms.Sorting;
+using Xunit;
+using System.Linq;
 
 namespace UnitTest.AlgorithmsTests
 {
-	public static class InsertionSortTest
-	{
-		public static void DoTest ()
-		{
-			ArrayList<int> list = new ArrayList<int> ();
+    public static class InsertionSortTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            var list1 = new ArrayList<int>
+            {
+                23,
+                42,
+                4,
+                16,
+                8,
+                15,
+                9,
+                55,
+                0,
+                34,
+                12,
+                2
+            };
+            var list2 = new List<long>
+            {
+                23,
+                42,
+                4,
+                16,
+                8,
+                15,
+                9,
+                55,
+                0,
+                34,
+                12,
+                2
+            };
 
-			list.Add (23);
-			list.Add (42);
-			list.Add (4);
-			list.Add (16);
-			list.Add (8);
-			list.Add (15);
-			list.Add (9);
-			list.Add (55);
-			list.Add (0);
-			list.Add (34);
-			list.Add (12);
-			list.Add (2);
+            // sort both lists 
+            list1.InsertionSort();
+            list2.InsertionSort();
 
-			Console.WriteLine ("Before Sort:\r\n" + list.ToHumanReadable() + "\r\n");
+            bool isListEqual = true;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] != list2[i])
+                {
+                    isListEqual = false;
+                    break;
+                }
+            }
 
-			list.InsertionSort ();
-
-			Console.WriteLine ("After Sort:\r\n" + list.ToHumanReadable() + "\r\n");
-
-
-			// ANOTHER LIST TO SORT
-
-			List<long> list2 = new List<long> ();
-
-			list2.Add (23);
-			list2.Add (42);
-			list2.Add (4);
-			list2.Add (16);
-			list2.Add (8);
-			list2.Add (15);
-			list2.Add (9);
-			list2.Add (55);
-			list2.Add (0);
-			list2.Add (34);
-			list2.Add (12);
-			list2.Add (2);
-
-			list2.InsertionSort ();
-		}
-	}
+            Assert.True(isListEqual);
+        }
+    }
 }
 
