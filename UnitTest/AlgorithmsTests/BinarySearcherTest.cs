30ea305771246b06edbb36a4c4cf8ef4f4f24596
kdimolikas <konstdimolikas@gmail.com>
2019-11-07 18:51:12 +0200

    Introduce test for the enumerator

--- UnitTest/AlgorithmsTests/BinarySearcherTest.cs
+++ UnitTest/AlgorithmsTests/BinarySearcherTest.cs
@@ -6,42 +6,70 @@ namespace UnitTest.AlgorithmsTests
 {
     public static class BinarySearcherTest
     {
-
-
         [Fact]
-        public static void BinarySearchTest()
+        public static void IntBinarySearchTest()
         {
             //list of ints
             IList<int> list = new List<int> { 9, 3, 7, 1, 6, 10 };
             IList<int> sortedList = new List<int> { 1, 3, 6, 7, 9, 10 };
-            BinarySearcher<int> intSearcher = new BinarySearcher<int>(list, Comparer<int>.Default);
             int numToSearch = 6;
-            int itemIndex = intSearcher.BinarySearch(numToSearch);
+            BinarySearcher<int> intSearcher = new BinarySearcher<int>(list, Comparer<int>.Default);
+            intSearcher.SearchFor(numToSearch);
+            int itemIndex = intSearcher.BinarySearch();
             int expectedIndex = sortedList.IndexOf(numToSearch);
 
             Assert.Equal(expectedIndex, itemIndex);
             Assert.Equal(numToSearch, intSearcher.Current);
 
             numToSearch = 20;
-            int itemNotExists = intSearcher.BinarySearch(numToSearch);
+            intSearcher.SearchFor(numToSearch);
+            int itemNotExists = intSearcher.BinarySearch();
             Assert.Equal(-1, itemNotExists);
+        }
 
-            intSearcher.Dispose();
-
+        [Fact]
+        public static void StringBinarySearchTest()
+        {
             //list of strings
-            IList<string> animals = new List<string> {"lion", "cat", "tiger", "bee", "sparrow"};
+            IList<string> animals = new List<string> { "lion", "cat", "tiger", "bee", "sparrow" };
             IList<string> sortedAnimals = new List<string> { "bee", "cat", "lion", "sparrow", "tiger" };
-            BinarySearcher<string> strSearcher = new BinarySearcher<string>(animals, Comparer<string>.Default);
             string itemToSearch = "bee";
-            int actualIndex = strSearcher.BinarySearch(itemToSearch);
+            BinarySearcher<string> strSearcher = new BinarySearcher<string>(animals, Comparer<string>.Default);
+            strSearcher.SearchFor(itemToSearch);
+            int actualIndex = strSearcher.BinarySearch();
             int expectedAnimalIndex = sortedAnimals.IndexOf(itemToSearch);
 
             Assert.Equal(expectedAnimalIndex, actualIndex);
             Assert.Equal(itemToSearch, strSearcher.Current);
 
-            strSearcher.Dispose();
+            itemToSearch = "shark";
+            strSearcher.SearchFor(itemToSearch);
+            int itemNotExists = strSearcher.BinarySearch();
+            Assert.Equal(-1, itemNotExists);
         }
 
+        [Fact]
+        public static void MoveNextTest()
+        {
+            IList<int> items = new List<int> {3, 5, 2, 6, 1, 4};
+            BinarySearcher<int> searcher = new BinarySearcher<int>(items, Comparer<int>.Default);
+            searcher.SearchFor(1);
+            IList<int> leftEnumeratedValues = new List<int> {3, 2, 1};
+            int i = 0;
+            while (searcher.MoveNext())
+            {
+                Assert.Equal(leftEnumeratedValues[i++], searcher.Current);
+            }
+
+            searcher.SearchFor(6);
+            IList<int> rightEnumeratedValues = new List<int> {3, 5, 6};
+            i = 0;
+            while (searcher.MoveNext())
+            {
+                Assert.Equal(rightEnumeratedValues[i++], searcher.Current);
+            }
+
+        }
 
         [Fact]
         public static void NullCollectionExceptionTest()
@@ -49,6 +77,5 @@ namespace UnitTest.AlgorithmsTests
             IList<int> list = null;
             Assert.Throws<System.NullReferenceException>(() => new BinarySearcher<int>(list, Comparer<int>.Default));
         }
-
     }
 }
